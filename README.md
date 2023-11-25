a03fbf167bcd7a86a7e0a34f8cc2bb4f539d4cd8
julianeon <julianeon@gmail.com>
2023-11-25 11:46:46 -0800

    military time

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Overview
 
+![clock screenshot](clock_ts.png)
+
 This is a simple React clock app made with TypeScript.
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -9,6 +11,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 ## Features
 
 This app the time digitally in the center of the screen, in the format of hours:minutes:seconds.
+
+Click on it to switch the time format display to military time. Click to revert it to regular time.
 
 ## How To Run
 
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,24 +1,45 @@
-// src/Clock.tsx
 import React, { useState, useEffect } from 'react';
+import './clock.css';
 
 const Clock: React.FC = () => {
-    const [time, setTime] = useState<string>('');
+    const [isMilitary, setIsMilitary] = useState<boolean>(false); // State to track view mode
+
+    // Function to get the current time string based on the view mode
+    const getCurrentTimeString = () => {
+        const now = new Date();
+        let hours = now.getHours();
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+
+        if (!isMilitary) {
+            // Convert to 12-hour format without AM/PM
+            hours = hours % 12;
+            hours = hours ? hours : 12; // the hour '0' should be '12'
+            return `${String(hours).padStart(2, '0')}:${minutes}:${seconds}`;
+        }
+
+        // 24-hour format
+        return `${String(hours).padStart(2, '0')}:${minutes}:${seconds}`;
+    };
+
+    const [time, setTime] = useState<string>(getCurrentTimeString());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const now = new Date();
-            const hours = String(now.getHours()).padStart(2, '0');
-            const minutes = String(now.getMinutes()).padStart(2, '0');
-            const seconds = String(now.getSeconds()).padStart(2, '0');
-            setTime(`${hours}:${minutes}:${seconds}`);
+            setTime(getCurrentTimeString());
         }, 1000);
 
         // Cleanup the interval on component unmount
         return () => clearInterval(intervalId);
-    }, []);
+    }, [isMilitary]);
+
+    // Function to toggle the view mode
+    const toggleView = () => {
+        setIsMilitary(!isMilitary);
+    };
 
     return (
-        <div id="clock">
+        <div id="clock" onClick={toggleView} style={{ color: isMilitary ? 'yellow' : '#7CB9E8' }}>
             {time}
         </div>
     );
